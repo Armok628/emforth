75e50481bc57c7a7e9f2958c0ad7009ef601f50b
Armok628 <Mark.Johnson.62899@tamu.edu>
2019-11-18 13:24:19 -0600

    More experimentation with double handling

--- builtins/double_arith.c
+++ builtins/double_arith.c
@@ -1,7 +1,18 @@
 #ifdef EMULATE_DOUBLES
-#define POP_DOUBLE(r) (r -= 2, r[0] << 4 * sizeof(cell_t) | r[1])
+const ucell_t lohalf = (1UL << 4 * sizeof(cell_t)) - 1;
+#define LOAD_DOUBLE(p) ((p)[0] << 4 * sizeof(cell_t) | (p)[1])
+#define STORE_DOUBLE(p,v) \
+	do { \
+		udcell_t a = (v); \
+		*(ucell_t *)&(p)[0] = a >> 4 * sizeof(cell_t); \
+		*(ucell_t *)&(p)[1] = a & lohalf; \
+	} while (0)
 #else
-#define POP_DOUBLE(r) (r -= 2, *(udcell_t *)&r[-2])
+#define LOAD_DOUBLE(p) (*(dcell_t *)(p))
+#define STORE_DOUBLE(p,v) \
+	do { \
+		*(udcell_t *)(p) = (udcell_t)(v); \
+	} while (0)
 #endif
 
 um_divmod_code: /*: UM/MOD ( um_divmod ) ;*/
@@ -12,8 +23,15 @@ um_divmod_code: /*: UM/MOD ( um_divmod ) ;*/
 		:"=d"(sp[-1]), "=a"(tos)
 		:"d"(sp[0]), "a"(sp[-1]), "r"(tos));
 #else
-	udcell_t d = POP_DOUBLE(sp);
+	udcell_t d = LOAD_DOUBLE(&sp[-2]);
+	sp -= 2;
 	PUSH(sp) = d % tos;
 	tos = d / tos;
 #endif
+	goto next;
+
+m_add_code: /*: M+ ( m_add ) ;*/
+	ASMLABEL(m_add_code);
+	STORE_DOUBLE(&sp[-2], LOAD_DOUBLE(&sp[-2]) + (ucell_t)tos);
+	tos = POP(sp);
 	goto next;
