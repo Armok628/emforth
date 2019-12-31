19dbafdabf941049fb5de79de57c7d60bbd3023c
Armok628 <Mark.Johnson.62899@gmail.com>
2019-12-31 14:29:54 -0600

    Unindent cell header

--- cell.h
+++ cell.h
@@ -8,20 +8,20 @@
 #define CELL16 (INTPTR_MAX == 0x7FFF)
 
 #if CELL64
-	typedef __int128_t dcell_t;
-	typedef __uint128_t udcell_t;
-	typedef int64_t cell_t;
-	typedef uint64_t ucell_t;
+typedef __int128_t dcell_t;
+typedef __uint128_t udcell_t;
+typedef int64_t cell_t;
+typedef uint64_t ucell_t;
 #elif CELL32
-	typedef int64_t dcell_t;
-	typedef uint64_t udcell_t;
-	typedef int32_t cell_t;
-	typedef uint32_t ucell_t;
+typedef int64_t dcell_t;
+typedef uint64_t udcell_t;
+typedef int32_t cell_t;
+typedef uint32_t ucell_t;
 #elif CELL16
-	typedef int32_t dcell_t;
-	typedef uint32_t udcell_t;
-	typedef int16_t cell_t;
-	typedef uint16_t ucell_t;
+typedef int32_t dcell_t;
+typedef uint32_t udcell_t;
+typedef int16_t cell_t;
+typedef uint16_t ucell_t;
 #else
 	#error
 #endif
