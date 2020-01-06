4ff24cc48a1b5e1f4217d912f3020b0bdddcc616
Armok628 <Mark.Johnson.62899@gmail.com>
2020-01-05 18:11:01 -0600

    Finish (?) wip_gen_dict

--- wip_gen_dict.tcl
+++ wip_gen_dict.tcl
@@ -33,6 +33,7 @@ directive \;       {compile EXIT; set ::state ""}
 directive POSTPONE {compile [word]}
 directive LITERAL  {compile "DOLIT" [pop]}
 directive \[']     {compile "DOLIT" &[word]}
+directive \[CHAR]  {compile "DOLIT" "'[string index [word] 0]'"}
 
 proc mark> {} {push [here]; compile "UNRESOLVED"}
 proc >resolve {} {set i [pop]; lset ::def($::state) $i [expr {[here]-$i}]}
@@ -49,7 +50,11 @@ directive REPEAT {compile  "BRANCH"; <resolve; >resolve}
 directive AGAIN  {compile  "BRANCH"; <resolve}
 directive UNTIL  {compile "0BRANCH"; <resolve}
 
-directive ( {while {[word] != ")"} continue}
+directive ? {while {[word] ne ":"} continue} ;# Ignore C ternary operators
+directive ( {while {[word] ne ")"} continue}
+
+directive /*: {set ::state [word]}
+directive \;*/ {set ::state ""}
 
 directive "" continue
 while {[llength $words]} {
@@ -66,10 +71,56 @@ while {[llength $words]} {
 	}
 }
 
-foreach name [array names def] {
-	puts "$name => $def($name)]"
+proc safename {name} {
+	# Generate a reasonable C token from a given Forth name
+	if {[string is entier $name]} {return $name}
+	set name [string tolower $name]
+	set name [string map [list \
+	! _exclam_	\" _quote_	\# _pound_	\
+	\$ _dollar_	% _pcent_	& _and_		\
+	' _tick_	( _lparen_	) _rparen_	\
+	* _star_	+ _plus_	, _comma_	\
+	- _dash_	. _dot_		/ _slash_	\
+	0 _zero_	1 _one_		2 _two_		\
+	: _colon_	\; _semi_			\
+	< _less_	= _equal_	> _more_	\
+	? _quest_	@ _at_				\
+	\[ _lbrkt_	\\ _bkslsh_	\] _rbrack_	\
+	^ _caret_	` _grave_			\
+	\{ _lbrace_	| _pipe_	\} _rbrace_	\
+	~ _tilde_					\
+	] $name]
+	set name [string map [list "__" "_"] $name]
+	return [string trim $name "_"]
 }
 
-# 	TODO
-# * Handle primitives defined in C with the words /*: and ;*/
-# * Define output procedures for dictionary links
+proc xt {name} {
+	if {[string is entier $name]} {
+		return "(void **)$name"
+	} else {
+		return "&[safename $name]_def.cf"
+	}
+}
+
+set ::prev "NULL"
+proc putlink {name} {
+	set def [lassign $::def($name) cf]
+	if {[info exists ::def($cf)]} {
+		set cf [xt [lindex $::def($cf) 0]]
+	}
+	set ctok [safename $name]
+	puts "static struct fthdef ${ctok}_def = {
+	.prev = $::prev,
+	.name = \"$name\",
+	.namelen = [string length $name],
+	// .cf = $cf,
+	.data = {[join [lmap w $def {xt $w}] ", "]},
+}"
+	set ::prev "&${ctok}_def"
+}
+
+foreach name [lsort [array names def]] {
+	putlink $name
+}
+
+# TODO: Revise builtins/* to accept new format
