c3a2d5e2c7eaec2528ec4b978e2c20dad06a285e
Armok628 <Mark.Johnson.62899@gmail.com>
2020-01-06 11:31:47 -0600

    Big refactor

--- wip_gen_dict.tcl
+++ wip_gen_dict.tcl
@@ -15,39 +15,13 @@ proc compile {args} {lappend ::def($::state) {*}$args; return}
 proc here {} {return [llength $::def($::state)]}
 proc swap {} {push {*}[lreverse [pop 2]]}
 
-proc directive {name body} {set ::directive($name) $body}
-
-directive :        {set ::state [word]; compile "DOCOL"}
-directive \;       {compile EXIT; set ::last $::state; set ::state ""}
-directive POSTPONE {compile [word]}
-directive LITERAL  {compile "DOLIT" [pop]}
-directive \[']     {compile "DOLIT" &[word]}
-directive \[CHAR]  {compile "DOLIT" [scan [string index [word] 0] %c]}
-directive IMMEDIATE {set ::flags($::last) "|MSB"}
+proc cast {n} {return "(void **)$n"}
+proc cells {n} {cast "($n*sizeof(cell_t))"}
 
 proc mark> {} {push [here]; compile "UNRESOLVED"}
-proc >resolve {} {set i [pop]; lset ::def($::state) $i [expr {[here]-$i}]}
+proc >resolve {} {set i [pop]; lset ::def($::state) $i [cells [expr {[here]-$i}]]}
 proc mark< {} {push [here]}
-proc <resolve {} {compile [expr {[pop]-[here]}]}
-
-directive IF   {compile "0BRANCH"; mark>}
-directive ELSE {compile  "BRANCH"; mark>; swap; >resolve}
-directive THEN {>resolve}
-
-directive BEGIN  {mark<}
-directive WHILE  {compile "0BRANCH"; mark>; swap}
-directive REPEAT {compile  "BRANCH"; <resolve; >resolve}
-directive AGAIN  {compile  "BRANCH"; <resolve}
-directive UNTIL  {compile "0BRANCH"; <resolve}
-
-directive CONSTANT {set ::def([word]) [list "DOCON" [pop]]}
-directive VARIABLE {set ::def([word]) [list "DOVAR" 0]}
-
-directive ? {while {[word] ne ":"} continue} ;# Ignore C ternary operators
-directive ( {while {[word] ne ")"} continue}
-
-directive /*: {set ::state [word]}
-directive \;*/ {set ::state ""}
+proc <resolve {} {compile [cells [expr {[pop]-[here]}]]}
 
 proc cid {name} {
 	# Generate a reasonable C identifier from a given Forth name
@@ -73,38 +47,60 @@ proc cid {name} {
 	set name [string map [list "__" "_"] $name]
 	return [string trim $name "_"]
 }
+proc xt {name} {return "&[cid $name].cf"}
+proc cf {name} {lindex $::def($name) 0}
 
-proc xt {name} {expr {[string is entier $name] ? "(void **)$name" : "&[cid $name].cf"}}
-proc flags {name} {expr {[info exists ::flags($name)] ? $::flags($name) : ""}}
-proc cf {name} {
-	set cf [lindex $::def($name) 0]
-	if {[info exists ::def($cf)]} {
-		return "&&[lindex $::def($cf) 0]"
-	} else {
-		return "&&$cf"
-	}
-}
+proc dir {name def} {set ::directive($name) $def}
+dir :         {set ::state [word]; compile [cf DOCOL]}
+dir \;        {compile [xt EXIT]; set ::last $::state; set ::state ""}
+dir POSTPONE  {compile [xt [word]]}
+dir LITERAL   {compile [xt DOLIT] [cast [pop]]}
+dir \[']      {compile [xt DOLIT] [xt [word]]}
+dir \[CHAR]   {compile [xt DOLIT] [cast [scan [string index [word] 0] %c]]}
+dir IMMEDIATE {set ::flags($::last) "|MSB"}
+
+dir IF   {compile [xt 0BRANCH]; mark>}
+dir ELSE {compile  [xt BRANCH]; mark>; swap; >resolve}
+dir THEN {>resolve}
+
+dir BEGIN  {mark<}
+dir WHILE  {compile [xt 0BRANCH]; mark>; swap}
+dir REPEAT {compile  [xt BRANCH]; <resolve; >resolve}
+dir AGAIN  {compile  [xt BRANCH]; <resolve}
+dir UNTIL  {compile [xt 0BRANCH]; <resolve}
+
+dir CONSTANT {set ::def([word]) [list [cf DOCON] [pop]]}
+dir VARIABLE {set ::def([word]) [list [cf DOVAR] 0]}
+
+dir ? {while {[word] ne ":"} continue} ;# Ignore C ternary operators
+dir ( {while {[word] ne ")"} continue}
+
+dir {} {continue}
 
 foreach name $argv {lappend words {*}[split [read [open $name]]]}
 if {![info exists words]} {set words [split [read stdin]]}
+foreach {m f c} [regexp -all -inline {/\*: (.*?) (.*?) {;\*/}} $words] {
+	set ::def($f) "&&$c"
+}
 
-directive "" continue
 while {[llength $words]} {
 	set word [word]
 	if {[info exists directive($word)]} {
 		eval $directive($word)
 	} elseif {$state ne ""} {
-		if {[string is double $word]} {
-			compile "DOLIT"
+		if {[string is entier $word]} {
+			compile [xt DOLIT] [cast $word]
+		} else {
+			compile [xt $word]
 		}
-		compile $word
-	} elseif {[info exists def($word)] && [lindex $def($word) 0] eq "DOVAR"} {
+	} elseif {[info exists def($word)] && [lindex $def($word) 0] eq [cf DOVAR]} {
 		lset def($word) 1 [pop]
 	} elseif {[string is entier $word]} {
 		push $word
 	}
 }
 
+proc flags {name} {expr {[info exists ::flags($name)] ? $::flags($name) : ""}}
 puts "static struct fthdef\n\t[join [lmap n [lsort [array names def]] {cid $n}] ",\n\t"];\n"
 set ::prev "NULL"
 foreach name [lsort -decreasing [array names def]] {
@@ -113,8 +109,8 @@ foreach name [lsort -decreasing [array names def]] {
 	.prev = $::prev,
 	.name = \"$name\",
 	.namelen = [string length $name][flags $name],
-	// .cf = [cf $name],
-	.data = {[join [lmap w $params {xt $w}] ", "]},
+	// .cf = $cf,
+	.data = {[join $params ", "]},
 }"
 	set ::prev "&[cid $name]"
 }
