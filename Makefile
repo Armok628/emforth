95c736999ce26080737b7669ca148a279552857c
mijohnson99 <Mark.Johnson.62899@tamu.edu>
2020-02-14 18:39:07 -0600

    Generate vocab.h automatically

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ CC = clang
 CFLAGS = -Os -g
 SRCS = engine/*.c
 
-a.out: main.c engine.c *.h
+a.out: main.c engine.c vocab.h fthutil.h
 	$(CC) $(CFLAGS) $< -o $@
+
+vocab.h: genvoc $(SRCS)
+	./genvoc $(SRCS) > $@
 
 engine.c: $(SRCS)
 	find $(SRCS) | sed 's/.*/#include "&"/' > $@
 
 .PHONY: clean cleanest
 clean:
-	rm -f a.out engine.c
+	rm -f a.out engine.c vocab.h
 cleanest: clean
 	rm -f .*~ *~ */.*~ */*~
--- fthutil.h
+++ fthutil.h
@@ -1,7 +1,7 @@
 #ifndef FTHUTIL_H
 #define FTHUTIL_H
 /*
- * 	Utilities for use in Forth primitives
+ * 	Utilities for use in the generation of Forth primitives
  */
 
 #include <stddef.h>
@@ -25,6 +25,15 @@ typedef int32_t dcell;
 typedef int16_t cell;
 typedef uint16_t ucell;
 #endif
+
+struct fthdef {
+	struct fthdef *prev;
+	char *name;
+	bool imm : 1;
+	cell len : 8*sizeof(cell)-1;
+	void *cf;
+	void **data[];
+};
 
 #define COUNT(a) (sizeof(a)/sizeof(a[0]))
 
@@ -66,5 +75,24 @@ typedef uint16_t ucell;
 		(z) = tmp; \
 	} while (0)
 
+// Used in the VOCAB X macro to generate fthdef structs.
+// The line filling in .cf is commented out because its value should be local to engine.
+// It should be filled in when engine is called with a NULL instruction pointer.
+#define DEF(cn,pr,nm,cf,...) \
+struct fthdef cn = { \
+	.prev = pr, \
+	.name = nm, \
+	.imm = 0, \
+	.len = COUNT(nm), \
+	/*.cf = cf,*/ \
+	.data = {__VA_ARGS__}, \
+};
+
+// Used in the VOCAB X macro to generate a list of code fields.
+#define CF(cn,pr,nm,cf,...) cf,
+
+// Used in the varargs of a VOCAB entry to compile code by hand.
+#define XT(cn) &cn##_d.cf
+#define LIT(n) XT(dolit),(void **)(n)
 
 #endif
